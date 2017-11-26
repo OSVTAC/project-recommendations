d0e91542b3381c3adddd8a8594ecf6bbbe92faa4
Chris Jerdonek <chris.jerdonek@gmail.com>
2017-11-25 21:23:39 -0800

    License the code in the repository under GPL-3.0+.
    This is per TAC's November 16, 2017 meeting.

--- _scripts/prep.py
+++ _scripts/prep.py
@@ -18,6 +18,30 @@ The script should be run with Python 3.6 or newer (because it uses
 f-strings, for example).
 """
 
+# prep.py script to prepare Markdown files for publishing with Jekyll.
+#
+# Copyright (C) 2017  Christopher Jerdonek
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Contact info:
+#
+# The author(s) can be reached at--
+#
+#   Christopher Jerdonek <chris.jerdonek@gmail.com>
+#
+
 import os
 import re
 
